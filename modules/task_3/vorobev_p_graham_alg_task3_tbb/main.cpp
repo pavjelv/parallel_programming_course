565a928623fd929b744fc3604c0cd25cde0bdff9
Pavel Vorobev <sayhello98123@gmail.com>
2019-05-10 20:28:18 +0300

    fixed tbb

--- modules/task_3/vorobev_p_graham_alg_task3_tbb/main.cpp
+++ modules/task_3/vorobev_p_graham_alg_task3_tbb/main.cpp
@@ -1,4 +1,5 @@
-// Copyright 2019 Pavel Vorobev
+// Copyright 2019 Vorobev Pavel
+
 
 #include <tbb/tbb.h>
 #include <tbb/parallel_sort.h>
@@ -11,18 +12,32 @@
 struct point {
     int x;
     int y;
-    friend bool operator < (point a, point c){
-        return a.x - a.y
-            + c.y - c.x
-            + c.x * a.y - c.y * a.x > 0;
-    }
 };
+
 
 bool moreOrEqualThan(point a, point c, point b) {
     return a.x * b.y - a.y * b.x
         + b.x * c.y - b.y * c.x
         + c.x * a.y - c.y * a.x <= 0;
 }
+
+
+class Comparator {
+ private:
+    point b;
+
+ public:
+    void setFirstPoint(point p) {
+        b.x = p.x;
+        b.y = p.y;
+    }
+
+    bool operator() (point a, point c) const {
+        return a.x * b.y - a.y * b.x
+            + b.x * c.y - b.y * c.x
+            + c.x * a.y - c.y * a.x > 0;
+    }
+};
 
 bool notEquals(point a, point b) {
     if (a.x != b.x)
@@ -42,6 +57,9 @@ int partition(std::vector<point> *a, int low, int high) {
     point pivot = a->at(low);
     int i = low + 1;
     for (int j = low + 1; j <= high; j++) {
+        /*if (a->at(j).x == pivot.x && a->at(j).y == pivot.y) {
+        continue;
+        }*/
         if (moreOrEqualThan(pivot, a->at(j), a->at(0))) {
             point tmp = a->at(i);
             a->at(i) = a->at(j);
@@ -57,17 +75,13 @@ int partition(std::vector<point> *a, int low, int high) {
 
 void quickSort(std::vector<point> *a, int low, int high) {
     if (low < high) {
-        int partIndex = partition(a, low, high);
+        int	partIndex = partition(a, low, high);
         quickSort(a, low, partIndex - 1);
         quickSort(a, partIndex + 1, high);
     }
 }
 
 int grahamScanLinear(std::vector<point> *a) {
-    point p;
-    p.x = 1;
-    p.y = 1;
-    a->push_back(p);
     point c = a->at(0);
     int m = 0;
     for (size_t i = 1; i < a->size(); i++) {
@@ -87,7 +101,7 @@ int grahamScanLinear(std::vector<point> *a) {
     a->at(m) = tmp;
     m = 1;
     quickSort(a, 1, a->size() - 1);
-    a->erase(a->begin());
+
     for (size_t i = 1; i < a->size(); i++) {
         if (notEquals(a->at(i), a->at(m))) {
             if (m >= 1) {
@@ -119,12 +133,15 @@ int grahamScanParallel(std::vector<point> *a) {
             }
         }
     }
-
     point tmp = a->at(0);
     a->at(0) = a->at(m);
     a->at(m) = tmp;
     m = 1;
-    tbb::parallel_sort(*a);
+
+    Comparator cmp;
+    cmp.setFirstPoint(a->at(0));
+    tbb::parallel_sort(a->begin() + 1, a->end(), cmp);
+
     for (size_t i = 1; i < a->size(); i++) {
         if (notEquals(a->at(i), a->at(m))) {
             if (m >= 1) {
@@ -142,7 +159,7 @@ int grahamScanParallel(std::vector<point> *a) {
     return m + 1;
 }
 
-int randomize(int value) {
+double randomize(double value) {
     double min = 1;
     double max = 4;
     return value + (static_cast<int> (min) + std::rand() %
@@ -151,12 +168,11 @@ int randomize(int value) {
 
 int main() {
     std::srand((unsigned)time(NULL));
-    double numOfPoints = 10000;
+    double numOfPoints = 10000000;
     double min = 1;
     double max = 10000;
     std::vector<point>* data = new std::vector<point>;
     std::vector<point>* parallelData = new std::vector<point>;
-    std::vector<point>* a = new std::vector<point>;
     for (size_t i = 0; i < numOfPoints; i++) {
         point p;
         p.x = randomize(static_cast<int> (min) + std::rand() %
@@ -165,13 +181,13 @@ int main() {
             (static_cast<int> (max) - static_cast<int> (min) + 1));
         data->push_back(p);
         parallelData->push_back(p);
-        a->push_back(p);
     }
     int num_threads = 4;
     tbb::task_scheduler_init init(num_threads);
-
     tbb::tick_count t1 = tbb::tick_count::now();
+
     int hullSize = grahamScanLinear(data);
+
     tbb::tick_count t2 = tbb::tick_count::now();
     double time = (t2 - t1).seconds();
     std::cout << std::endl << "linear time: " << time << std::endl;
@@ -179,20 +195,21 @@ int main() {
         std::cout << "( " << data->at(i).x << ", " << data->at(i).y << ") ";
     }
 
-    std::vector<point>().swap(*data);
-
     tbb::tick_count t3 = tbb::tick_count::now();
+
     int size = grahamScanParallel(parallelData);
+
     tbb::tick_count t4 = tbb::tick_count::now();
     double parallelTime = (t4 - t3).seconds();
     std::cout << std::endl <<"parallel time: " << parallelTime << std::endl;
     for (int i = 0; i < size; i++) {
         std::cout << "( " << parallelData->at(i).x <<
-            ", " << parallelData->at(i).y << ") ";
+                    ", " << parallelData->at(i).y << ") ";
     }
-    std::cout << std::endl;
 
-    std::vector<point>().swap(*parallelData);
+    std::cout << std::endl;
+    delete data;
+    delete parallelData;
     return 0;
 }
 
